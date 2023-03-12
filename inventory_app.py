dde59ee0fe91782ddad310521f24755d3a2c5cad
huannguyen <huan2931@gmail.com>
2023-03-12 09:06:23 -0700

    fixing gui

--- inventory_app.py
+++ inventory_app.py
@@ -28,16 +28,16 @@ def connect_to_firebase():
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(944, 695)
+        MainWindow.resize(1056, 811)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.logo_HCMUTE = QtWidgets.QLabel(self.centralwidget)
-        self.logo_HCMUTE.setGeometry(QtCore.QRect(250, 0, 451, 111))
+        self.logo_HCMUTE.setGeometry(QtCore.QRect(250, 10, 451, 111))
         self.logo_HCMUTE.setText("")
         self.logo_HCMUTE.setPixmap(QtGui.QPixmap(":/newPrefix/Logo.png"))
         self.logo_HCMUTE.setObjectName("logo_HCMUTE")
         self.group_img = QtWidgets.QGroupBox(self.centralwidget)
-        self.group_img.setGeometry(QtCore.QRect(160, 260, 651, 391))
+        self.group_img.setGeometry(QtCore.QRect(160, 260, 651, 491))
         font = QtGui.QFont()
         font.setFamily("URW Bookman [urw]")
         font.setPointSize(14)
@@ -47,7 +47,7 @@ class Ui_MainWindow(object):
         self.group_img.setFont(font)
         self.group_img.setObjectName("group_img")
         self.result_img = QtWidgets.QLabel(self.group_img)
-        self.result_img.setGeometry(QtCore.QRect(0, 30, 651, 391))
+        self.result_img.setGeometry(QtCore.QRect(110, 40, 448, 448))
         self.result_img.setObjectName("result_img")
         self.group_info = QtWidgets.QGroupBox(self.centralwidget)
         self.group_info.setGeometry(QtCore.QRect(160, 120, 651, 141))
@@ -83,12 +83,32 @@ class Ui_MainWindow(object):
         self.IS_index = QtWidgets.QLabel(self.group_info)
         self.IS_index.setGeometry(QtCore.QRect(260, 80, 111, 41))
         self.IS_index.setObjectName("IS_index")
-        self.avalible_index = QtWidgets.QLabel(self.group_info)
-        self.avalible_index.setGeometry(QtCore.QRect(460, 80, 111, 41))
+        self.avalible_index = QtWidgets.QProgressBar(self.group_info)
+        self.avalible_index.setGeometry(QtCore.QRect(460, 90, 118, 23))
+        self.avalible_index.setProperty("value", 24)
         self.avalible_index.setObjectName("avalible_index")
+        self.TimeBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.TimeBox.setGeometry(QtCore.QRect(820, 140, 201, 121))
+        font = QtGui.QFont()
+        font.setFamily("URW Bookman")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.TimeBox.setFont(font)
+        self.TimeBox.setObjectName("TimeBox")
+        self.time = QtWidgets.QLabel(self.TimeBox)
+        self.time.setGeometry(QtCore.QRect(10, 40, 210, 71))
+        font = QtGui.QFont()
+        font.setFamily("Sans")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.time.setFont(font)
+        self.time.setObjectName("time")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 944, 20))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1056, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -98,16 +118,20 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def update_data(self, OOS, IS, avaliable, img_name):
+    #******************************************************
+    #my code here
+    #******************************************************
+    def update_data(self, OOS, IS, avaliable, img_name,current_time):
         self.OOS_index.setText(str(OOS))
         self.IS_index.setText(str(IS))
-        self.avalible_index.setText(str(avaliable) + "%")
+        self.time.setText(str(current_time))
+        self.avalible_index.setProperty("value",float(avaliable)*100)
         #set image
         pixmap = QtGui.QPixmap(img_name)
-        self.h = self.result_img.width()
-        self.w = self.result_img.height()
-        print('w: ' + str(self.w) +'h:'+ str(self.h))
-        self.result_img.setPixmap(pixmap.scaled(QtCore.QSize(self.w,self.h),QtCore.Qt.KeepAspectRatio,QtCore.Qt.SmoothTransformation))
+        self.h = self.result_img.width() - 10
+        self.w = self.result_img.height() - 10
+        self.result_img.setPixmap(pixmap.scaled(QtCore.QSize(self.w,self.h),
+                                QtCore.Qt.KeepAspectRatio))
         return
 
     def download_image(self,img_name):
@@ -125,8 +149,9 @@ class Ui_MainWindow(object):
         self.In_stock = self.json_data['In-stock']
         self.avaliable = self.json_data['In-stock_avalivle']
         self.img_name = self.json_data['img_output']
+        self._time = self.json_data['time']
         self.download_image(self.img_name)
-        self.update_data(self.OOS,self.In_stock,self.avaliable,self.img_name)
+        self.update_data(self.OOS,self.In_stock,self.avaliable,self.img_name,self._time)
         print("firebase_realtime_callback end !")
         return
     
@@ -135,18 +160,23 @@ class Ui_MainWindow(object):
         self.ref = db.reference('/').listen(self.firebase_realtime_callback)
         return
 
+    #******************************************************
+    #End my code 
+    #******************************************************
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.group_img.setTitle(_translate("MainWindow", "shelf"))
-        self.result_img.setText(_translate("MainWindow", "result_img"))
+        self.result_img.setText(_translate("MainWindow", "Image"))
         self.group_info.setTitle(_translate("MainWindow", "Index"))
         self.OOS_lable.setText(_translate("MainWindow", "Out of stock"))
         self.IS_lable.setText(_translate("MainWindow", "In stock"))
         self.avalible_lable.setText(_translate("MainWindow", "Available in shelf"))
         self.OOS_index.setText(_translate("MainWindow", "0"))
         self.IS_index.setText(_translate("MainWindow", "0"))
-        self.avalible_index.setText(_translate("MainWindow", "0.00%"))
+        self.TimeBox.setTitle(_translate("MainWindow", "Time"))
+        self.time.setText(_translate("MainWindow", "time"))
 
 
 
@@ -158,8 +188,8 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.firebase_realtime()
     ui.setupUi(MainWindow)
+    ui.firebase_realtime()
     MainWindow.show()
     sys.exit(app.exec_())
     return
